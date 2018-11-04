9afecdef0736e4730a9523f4e06a3aa1d3e3ce80
Payson Wallach <paysonwallach@icloud.com>
2018-11-04 20:38:02 +0100

    Updated applications

--- install-user-applications.sh
+++ install-user-applications.sh
@@ -8,11 +8,8 @@ echo "License key located in iCloud://Licenses/Dash.license"
 # Download AppCleaner
 echo "Download AppCleaner from https://freemacsoft.net/appcleaner/"
 
-# Download Hyper
-echo "Download Hyper from https://releases.hyper.is/download/mac"
-
 # Download Spectacle
 echo "Download Spectacle from https://www.spectacleapp.com"
 
 # Download Spotify
-echo "Download "
+echo "Download Spotify from https://www.spotify.com/download/mac/"
