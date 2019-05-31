b7bdc4a0c79c9c435a94fffe3b13a1a5c1db62a7
Payson Wallach <paysonwallach@icloud.com>
2019-05-31 16:58:47 +0200

    Launch tmux on opening of interactive shell

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,4 +1,11 @@
-### Aliases ###
+# Launch tmux upon opening of new terminal
+if status is-interactive
+and not set -q TMUX
+    command tmux new-session \; new-window "tmux set-option -ga terminal-overrides \",$TERM:Tc\"; tmux detach"
+    command tmux attach
+end
+
+#--- Aliases ---
 
 # Dotdrop
 alias dotdrop '/home/paysonwallach/.dotfiles/dotdrop.sh --cfg=/home/paysonwallach/.dotfiles/config.yaml'
@@ -6,7 +13,8 @@ alias dotdrop '/home/paysonwallach/.dotfiles/dotdrop.sh --cfg=/home/paysonwallac
 # Trash
 alias rm 'echo -e "This is not the command you\'re looking for.\n\nIf you\'re sure you\'d like to use \'rm\',\nprepend with \'command\'."; false'
 
-### Environment ###
+#--- Environment ---
 
 # Pyenv
 set -x PYENV_ROOT $HOME/.pyenv
+
