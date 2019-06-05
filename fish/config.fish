97c3fef7849899950edef24ee5b39fefed7d98aa
Payson Wallach <paysonwallach@icloud.com>
2019-06-05 16:40:14 +0200

    Removed exporting of environmental variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,16 @@
-set -x PYENV_ROOT $HOME/.pyenv
+# Launch tmux upon opening of new terminal
+if status is-interactive
+and not set -q TMUX
+    command tmux new-session \; new-window "tmux set-option -ga terminal-overrides \",$TERM:Tc\"; tmux detach"
+    command tmux attach
+end
 
-set PATH /usr/local/opt/ruby/bin ~/.bin/ $PATH
 
+# --- Aliases ---
+
+# Dotdrop
+alias dotdrop '/Users/paysonwallach/.dotfiles/dotdrop.sh'
+
+
+# --- iTerm2 Shell Integration ---
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
