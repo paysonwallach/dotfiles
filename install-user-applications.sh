cd024d071bed9ed6f84c68ca051724ca66f30861
Payson Wallach <paysonwallach@icloud.com>
2019-07-29 19:45:57 -0700

    Fixed line continuation

--- install-user-applications.sh
+++ install-user-applications.sh
@@ -2,26 +2,26 @@
 
 echo "Installing App Store applications..."
 
-mas install \
+mas install
 # --- Creative ---
 824183456 \ # Affinity Photo
 824171161 \ # Affinity Designer
 # --- Development ---
 497799835 \ # Xcode
 # --- Office ---
-477670270 \ # 2Do
-1091189122 \ # Bear
-409201541 \ # Pages
-409203825 \ # Numbers
-409183694 \ # Keynote
-# --- Utilities ---
-412529613 \ # Cinch
-1397180934 \ # Dark Mode for Safari
-1335413823 \ # Ka-Block!
-928940999 \ # PasteBox
-1160374471 \ # PiPifier
-1185307985 \ # Soothe
-1000397973 \ # Wallcat
+477670270 \ # 2Do \
+1091189122 \ # Bear \
+409201541 \ # Pages \
+409203825 \ # Numbers \
+409183694 \ # Keynote \
+# --- Utilities --- \
+412529613 \ # Cinch \
+1397180934 \ # Dark Mode for Safari \
+1335413823 \ # Ka-Block! \
+928940999 \ # PasteBox \
+1160374471 \ # PiPifier \
+1185307985 \ # Soothe \
+1000397973 \ # Wallcat \
 
 echo "Download Dash from "
 echo "Sync folder located in iCloud://Documents/Documentation/Dash"
