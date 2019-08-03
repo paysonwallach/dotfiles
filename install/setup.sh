aba2ed427e701d86fff3e0e29bf7224d17810719
Payson Wallach <paysonwallach@icloud.com>
2019-08-02 22:45:34 -0700

    Removed redundant 'symlink.sh' and renamed 'config.sh' to 'system-preferences.sh'

--- install/setup.sh
+++ install/setup.sh
@@ -2,17 +2,14 @@
 
 ROOT="$(pwd)"
 
-# Ask for the administrator password upfront
-echo "Enter sudo password:"
+# Ask for an administrator password upfront
+echo "Enter sudo password: "
 sudo -v
 
 # Keep-alive: update existing sudo time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null
 
-chflags hidden $ROOT
-
-"$ROOT/symlink.sh"
-"$ROOT/config.sh"
+"$ROOT/system-preferences.sh"
 "$ROOT/install-development-tools.sh"
 "$ROOT/install-user-applications.sh"
 
