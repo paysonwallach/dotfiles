c420f345aa1becfe48160e0e49da4a1eeb040d14
Payson Wallach <paysonwallach@icloud.com>
2019-08-02 22:44:07 -0700

    Fixed bash line-continuation

--- install/install-user-applications.sh
+++ install/install-user-applications.sh
@@ -9,26 +9,16 @@ mas install
 # --- Development ---
 497799835 \ # Xcode
 # --- Office ---
-477670270 \ # 2Do \
-1091189122 \ # Bear \
-409201541 \ # Pages \
-409203825 \ # Numbers \
-409183694 \ # Keynote \
-# --- Utilities --- \
-412529613 \ # Cinch \
-1397180934 \ # Dark Mode for Safari \
-1335413823 \ # Ka-Block! \
-928940999 \ # PasteBox \
-1160374471 \ # PiPifier \
-1185307985 \ # Soothe \
-1000397973 \ # Wallcat \
-
-echo "Download Dash from "
-echo "Sync folder located in iCloud://Documents/Documentation/Dash"
-echo "License key located in iCloud://Licenses/Dash.license"
-
-echo "Download AppCleaner from https://freemacsoft.net/appcleaner/"
-
-echo "Download Spectacle from https://www.spectacleapp.com"
-
-echo "Download Spotify from https://www.spotify.com/download/mac/"
+477670270 \ # 2Do
+1091189122 \ # Bear
+409201541 \ # Pages
+409203825 \ # Numbers
+409183694 \ # Keynote
+# --- Utilities ---
+412529613 \ # Cinch
+1397180934 \ # Dark Mode for Safari
+1335413823 \ # Ka-Block!
+928940999 \ # PasteBox
+1160374471 \ # PiPifier
+1185307985 \ # Soothe
+1000397973 \ # Wallcat
