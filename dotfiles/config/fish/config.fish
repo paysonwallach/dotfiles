c720eede144ce566f7b7e614146d81a07ebb2205
Payson Wallach <paysonwallach@icloud.com>
2019-09-03 23:02:22 -0700

    Added virtualfish

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -5,7 +5,11 @@ and not set -q TMUX
     command tmux attach
 end
 
-#--- Aliases ---
+# Source virtualfish
+eval (python3 -m virtualfish)
+
+
+# --- Aliases ---
 
 # Dotdrop
 alias dotdrop '/home/paysonwallach/.dotfiles/dotdrop.sh --cfg=/home/paysonwallach/.dotfiles/config.yaml'
@@ -17,4 +21,3 @@ alias rm 'echo -e "This is not the command you\'re looking for.\n\nIf you\'re su
 
 # Pyenv
 set -x PYENV_ROOT $HOME/.pyenv
-
