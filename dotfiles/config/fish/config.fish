f2aca86dc0f432987a5073113666b110431dabe1
Payson Wallach <paysonwallach@icloud.com>
2019-09-29 23:53:06 -0500

    Removed `rm` alias

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -14,9 +14,6 @@ eval (python3 -m virtualfish)
 # Dotdrop
 alias dotdrop '/home/paysonwallach/.dotfiles/dotdrop.sh --cfg=/home/paysonwallach/.dotfiles/config.yaml'
 
-# Trash
-alias rm 'echo -e "This is not the command you\'re looking for.\n\nIf you\'re sure you\'d like to use \'rm\',\nprepend with \'command\'."; false'
-
 #--- Environment ---
 
 # Pyenv
