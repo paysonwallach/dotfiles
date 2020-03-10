3361db661892c5876919c357d1906586f191bb21
Payson Wallach <paysonwallach@icloud.com>
2020-03-09 18:24:32 -0700

    Normalize comments

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -1,18 +1,17 @@
-# Launch tmux upon opening of new terminal
+# launch tmux upon opening of new terminal
 if status is-interactive
 and not set -q TMUX
     command tmux new-session \; new-window "tmux set-option -ga terminal-overrides \",$TERM:Tc\"; tmux detach"
     command tmux attach
 end
 
-# Source virtualfish
 eval (python3 -m virtualfish)
 
 
 
 
+#--- environment ---
 
-#--- Environment ---
+# pyenv
 
-# Pyenv
 set -x PYENV_ROOT $HOME/.pyenv
