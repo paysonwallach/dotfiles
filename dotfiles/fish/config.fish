cf6672660f8400030e574eb769602ef44ba22a59
Payson Wallach <paysonwallach@icloud.com>
2020-03-10 13:35:25 -0700

    Refactor `dotdrop` alias into a lazily-loaded function

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -18,11 +18,5 @@ status --is-interactive; and source (pyenv init -|psub)
 eval (python3 -m virtualfish)
 
 
-# --- aliases ---
-
-# dotdrop
-alias dotdrop "$HOME/.dotfiles/dotdrop.sh"
-
-
 # --- iTerm2 shell integration ---
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
