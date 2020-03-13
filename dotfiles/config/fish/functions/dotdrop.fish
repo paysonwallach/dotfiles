fee9a6c89fdbca847b33665b759082c1c0ad9ae9
Payson Wallach <paysonwallach@icloud.com>
2020-03-12 19:32:09 -0700

    Set local path to virtual environment's interpreter

--- dotfiles/config/fish/functions/dotdrop.fish
+++ dotfiles/config/fish/functions/dotdrop.fish
@@ -1,3 +1,4 @@
 function dotdrop
-    fish -c "$HOME/.dotfiles/dotdrop.sh $argv -c $HOME/.dotfiles/config.yaml"
+    set -lx PATH "$HOME/.local/share/virtualenvs/dotdrop/bin" $PATH
+    "$HOME/.dotfiles/dotdrop.sh" $argv -c "$HOME/.dotfiles/config.yaml"
 end
