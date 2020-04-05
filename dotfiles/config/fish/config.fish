b0f5c58bf29314376f44b50bd9466da9a064144d
Payson Wallach <payson@paysonwallach.com>
2020-04-04 21:02:30 -0700

    Share aliases between shells with a separate file

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -16,5 +16,26 @@ status --is-interactive; and source (pyenv init -|psub)
 # source virtualfish
 eval (python3 -m virtualfish)
 
-# source zoxide
-zoxide init fish | source
+# --- aliases ---
+if status is-login
+    if test -f $HOME/.aliases
+        source $HOME/.aliases
+
+        # export aliases for lazy-loading in child shells
+        for alias in (grep -o "^alias \b\w+\b" $HOME/.aliases | cut -c 7-)
+            funcsave $alias
+        end
+    end
+
+    if test -f $HOME/.functions.fish
+        source $HOME/.functions.fish
+
+        # export functions for lazy-loading in child shells
+        for func in (grep -o "function \b\w+\b" $HOME/.aliases.fish | cut -c 10-)
+            funcsave $func
+        end
+    end
+end
+
+# --- environment ---
+nvm use default > /dev/null
